469c0c12e10cd13b698c0d3bbbd7f839c725580e
FabricioSobrinho <fabriciomails3@gmail.com>
2024-01-23 15:08:29 -0400

    test: testing the sanity of features

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using ChessGame.Board;
+using ChessGame.Chess;
 
 namespace ChessGame
 {
@@ -7,6 +8,8 @@ namespace ChessGame
         public static void Main(string[] args)
         {
             GameBoard board = new GameBoard(8, 8);
+
+            board.PutPiece(new King(Color.Black, board), new Position(1, 0));
 
             Screen.ShowScreen(board);
         }
