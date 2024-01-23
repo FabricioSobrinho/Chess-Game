c4d732332f2ea7a05c7ca232845545f68d13b7df
FabricioSobrinho <fabriciomails3@gmail.com>
2024-01-23 15:52:03 -0400

    Feat: Adding colors to diferent pieces

--- Screen.cs
+++ Screen.cs
@@ -8,6 +8,7 @@ namespace ChessGame
         {
             for (int i = 0; i < board.Rows; i++)
             {
+                Console.Write(8 - i + "| ");
                 for (int j = 0; j < board.Columns; j++)
                 {
                     if (board.ReturnPiece(i, j) == null)
@@ -16,10 +17,27 @@ namespace ChessGame
                     }
                     else
                     {
-                        Console.Write(board.ReturnPiece(i, j) + " ");
+                        ShowPiece(board.ReturnPiece(i, j));
                     }
                 }
-                Console.WriteLine("");
+                Console.WriteLine("|");
+            }
+            Console.WriteLine("   a b c d e f g h");
+        }
+
+        public static void ShowPiece(Piece piece)
+        {
+            if (piece.Color  == Color.White)
+            {
+                Console.Write(piece + " ");
+            } else
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                Console.Write(piece + " ");
+
+                Console.ForegroundColor = aux;
             }
         }
     }
