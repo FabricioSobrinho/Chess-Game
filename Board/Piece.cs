38d64bb743cde72f3db7d531ebd28b4353f44564
FabricioSobrinho <fabriciomails3@gmail.com>
2024-01-23 14:00:28 -0400

    Feat: Created the pieces class

---
+++ Board/Piece.cs
@@ -0,0 +1,20 @@
+﻿
+namespace ChessGame.Board
+{
+    internal class Piece
+    {
+        public Position? Position { get; set; }
+        public Color Color { get; set; }
+        public int MoveCount { get; protected set; }
+        public GameBoard? Board { get; protected set; }
+
+        public Piece() { }
+        public Piece(Position? position, Color color, GameBoard? board)
+        {
+            Position = position;
+            Color = color;
+            MoveCount = 0;
+            Board = board;
+        }
+    }
+}
