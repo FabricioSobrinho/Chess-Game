9f9b7dd8e1d174657efe4134299c22cbfc469d38
FabricioSobrinho <fabriciomails3@gmail.com>
2024-01-23 14:01:08 -0400

    Feat: created the GameBoard Class

---
+++ Board/GameBoard.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessGame.Board
+{
+    internal class GameBoard
+    {
+        public int Rows { get; set; }
+        public int Columns { get; set; }
+        public Piece[,]? Pieces { get; set; }
+        public GameBoard() { }
+        public GameBoard(int rows, int columns)
+        {
+            Rows = rows;
+            Columns = columns;
+            Pieces = new Piece[Rows, Columns];
+        }
+    }
+}
