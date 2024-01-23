73d3efbfdc1f9ed2931c2a8eb705eadf965c76e5
FabricioSobrinho <fabriciomails3@gmail.com>
2024-01-23 15:05:41 -0400

    Feat: Created the first pieces of the game

---
+++ Chess/King.cs
@@ -0,0 +1,17 @@
+﻿using ChessGame.Board;
+
+namespace ChessGame.Chess
+{
+    internal class King : Piece
+    {
+        public King( Color color, GameBoard board)
+            : base(color, board)
+        {
+        }
+
+        public override string ToString()
+        {
+            return "R";
+        }
+    }
+}
---
+++ Chess/Tower.cs
@@ -0,0 +1,17 @@
+﻿using ChessGame.Board;
+
+namespace ChessGame.Chess
+{
+    internal class Tower : Piece
+    {
+        public Tower(Color color, GameBoard board)
+            : base(color, board)
+        {
+        }
+
+        public override string ToString()
+        {
+            return "T";
+        }
+    }
+}
