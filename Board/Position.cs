e44a1640722cf3f91c08a7caddc1255b000966b2
FabricioSobrinho <fabriciomails3@gmail.com>
2024-01-23 13:41:48 -0400

    Feat: Created the position class

---
+++ Board/Position.cs
@@ -0,0 +1,21 @@
+﻿
+namespace ChessGame.Board
+{
+    internal class Position
+    {
+        public int Line { get; set; }
+        public int Column { get; set; }
+
+        public Position() { }
+        public Position(int line, int column)
+        {
+            Line = line;
+            Column = column;
+        }
+
+        public override string ToString()
+        {
+            return Line + "," + Column;
+        }
+    }
+}
