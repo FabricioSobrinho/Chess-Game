1b6a2ea2880a70ecdb376e69fe48ddb894af5c6f
FabricioSobrinho <fabriciomails3@gmail.com>
2024-01-23 14:16:23 -0400

    Feat: Started to implement the show board method

---
+++ Screen.cs
@@ -0,0 +1,26 @@
+﻿using ChessGame.Board;
+
+namespace ChessGame
+{
+    internal class Screen
+    {
+        public static void ShowScreen(GameBoard board)
+        {
+            for (int i = 0; i < board.Rows; i++)
+            {
+                for (int j = 0; j < board.Columns; j++)
+                {
+                    if (board.ReturnPiece(i, j) == null)
+                    {
+                        Console.Write("- ");
+                    }
+                    else
+                    {
+                        Console.Write(board.ReturnPiece(i, j) + " ");
+                    }
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
