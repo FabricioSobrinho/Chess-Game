d4def85d11d2cebf3476f8764d191e4544a9054a
FabricioSobrinho <fabriciomails3@gmail.com>
2024-01-25 16:05:03 -0400

    Chore: Minnor fixes

--- Board/Piece.cs
+++ Board/Piece.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace ChessGame.Board
 {
-    internal class Piece
+    internal abstract class Piece
     {
         public Position? Position { get; set; }
         public Color Color { get; protected set; }
@@ -21,5 +21,7 @@ namespace ChessGame.Board
         {
             MoveCount++;
         }
+
+        public abstract bool[,] PossibleMoves();
     }
 }
