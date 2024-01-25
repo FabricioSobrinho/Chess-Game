e639b9c6aee41327e96883c5bc793b8f5b518968
FabricioSobrinho <fabriciomails3@gmail.com>
2024-01-25 15:53:50 -0400

    Feat: added the set position values method

--- Board/Position.cs
+++ Board/Position.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.Data.Common;
+
 namespace ChessGame.Board
 {
     internal class Position
@@ -6,8 +8,13 @@ namespace ChessGame.Board
         public int Row { get; set; }
         public int Column { get; set; }
 
-        public Position() { }
         public Position(int row, int column)
+        {
+            Row = row;
+            Column = column;
+        }
+
+        public void SetValues(int row, int column)
         {
             Row = row;
             Column = column;
