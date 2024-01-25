8ce9bd31db17da129c1c6c9705f5fa9f4bf70419
FabricioSobrinho <fabriciomails3@gmail.com>
2024-01-25 16:05:47 -0400

    Feat: Added the showing next moves features

--- Chess/King.cs
+++ Chess/King.cs
@@ -4,7 +4,7 @@ namespace ChessGame.Chess
 {
     internal class King : Piece
     {
-        public King( Color color, GameBoard board)
+        public King(Color color, GameBoard board)
             : base(color, board)
         {
         }
@@ -12,6 +12,77 @@ namespace ChessGame.Chess
         public override string ToString()
         {
             return "R";
+        }
+        private bool CanMove(Position position)
+        {
+            Piece piece = Board!.Piece(position);
+
+            return piece == null || piece.Color != Color;
+        }
+
+        public override bool[,] PossibleMoves()
+        {
+            bool[,] matrix = new bool[Board.Rows, Board.Columns];
+
+            Position nextPosition = new Position(0, 0);
+
+            // up
+            nextPosition.SetValues(Position.Row - 1, Position.Column);
+            if (Board.ValidPosition(nextPosition) && CanMove(nextPosition))
+            {
+                matrix[nextPosition.Row, nextPosition.Column] = true;
+            }
+
+            // ne
+            nextPosition.SetValues(Position.Row - 1, Position.Column + 1);
+            if (Board.ValidPosition(nextPosition) && CanMove(nextPosition))
+            {
+                matrix[nextPosition.Row, nextPosition.Column] = true;
+            }
+
+            // right 
+            nextPosition.SetValues(Position.Row, Position.Column + 1);
+            if (Board.ValidPosition(nextPosition) && CanMove(nextPosition))
+            {
+                matrix[nextPosition.Row, nextPosition.Column] = true;
+            }
+
+            // se
+            nextPosition.SetValues(Position.Row + 1, Position.Column + 1);
+            if (Board.ValidPosition(nextPosition) && CanMove(nextPosition))
+            {
+                matrix[nextPosition.Row, nextPosition.Column] = true;
+            }
+
+            // down
+            nextPosition.SetValues(Position.Row + 1, Position.Column);
+            if (Board.ValidPosition(nextPosition) && CanMove(nextPosition))
+            {
+                matrix[nextPosition.Row, nextPosition.Column] = true;
+            }
+
+            // so
+            nextPosition.SetValues(Position.Row + 1, Position.Column - 1);
+            if (Board.ValidPosition(nextPosition) && CanMove(nextPosition))
+            {
+                matrix[nextPosition.Row, nextPosition.Column] = true;
+            }
+
+            // left
+            nextPosition.SetValues(Position.Row, Position.Column - 1);
+            if (Board.ValidPosition(nextPosition) && CanMove(nextPosition))
+            {
+                matrix[nextPosition.Row, nextPosition.Column] = true;
+            }
+
+            // no
+            nextPosition.SetValues(Position.Row - 1, Position.Column - 1);
+            if (Board.ValidPosition(nextPosition) && CanMove(nextPosition))
+            {
+                matrix[nextPosition.Row, nextPosition.Column] = true;
+            }
+
+            return matrix;
         }
     }
 }
--- Chess/Tower.cs
+++ Chess/Tower.cs
@@ -9,6 +9,77 @@ namespace ChessGame.Chess
         {
         }
 
+        private bool CanMove(Position position)
+        {
+            Piece piece = Board.Piece(position);
+
+            return piece == null || piece.Color != Color;
+        }
+
+        public override bool[,] PossibleMoves()
+        {
+            bool[,] matrix = new bool[Board.Rows, Board.Columns];
+
+            Position nextPosition = new Position(0, 0);
+
+            // up
+            nextPosition.SetValues(Position.Row - 1, Position.Column);
+            while (Board.ValidPosition(nextPosition) && CanMove(nextPosition))
+            {
+                matrix[nextPosition.Row, nextPosition.Column] = true;
+
+                if (Board.Piece(nextPosition) != null && Board.Piece(nextPosition).Color != Color)
+                {
+                    break;
+                }
+
+                nextPosition.Row = nextPosition.Row - 1;
+            }
+
+            // down
+            nextPosition.SetValues(Position.Row + 1, Position.Column);
+            while (Board.ValidPosition(nextPosition) && CanMove(nextPosition))
+            {
+                matrix[nextPosition.Row, nextPosition.Column] = true;
+
+                if (Board.Piece(nextPosition) != null && Board.Piece(nextPosition).Color != Color)
+                {
+                    break;
+                }
+
+                nextPosition.Row = nextPosition.Row + 1;
+            }
+
+            // left
+            nextPosition.SetValues(Position.Row, Position.Column - 1);
+            while (Board.ValidPosition(nextPosition) && CanMove(nextPosition))
+            {
+                matrix[nextPosition.Row, nextPosition.Column] = true;
+
+                if (Board.Piece(nextPosition) != null && Board.Piece(nextPosition).Color != Color)
+                {
+                    break;
+                }
+
+                nextPosition.Column = nextPosition.Column - 1;
+            }
+
+            // Right
+            nextPosition.SetValues(Position.Row, Position.Column + 1);
+            while (Board.ValidPosition(nextPosition) && CanMove(nextPosition))
+            {
+                matrix[nextPosition.Row, nextPosition.Column] = true;
+
+                if (Board.Piece(nextPosition) != null && Board.Piece(nextPosition).Color != Color)
+                {
+                    break;
+                }
+
+                nextPosition.Column = nextPosition.Column + 1;
+            }
+
+            return matrix;
+        }
         public override string ToString()
         {
             return "T";
